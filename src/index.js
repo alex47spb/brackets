56383ac83ba5aa0fdf9188dcd784d5362d04913e
alex47spb <heap.styx@mail.ru>
2020-10-12 18:06:38 +0300

    fix: identilal

--- src/index.js
+++ src/index.js
@@ -1,50 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-
     if (str.length % 2 !== 0) return false;
 
     var stek = [];
-    
+
     for (var iStr = 0; iStr < str.length; iStr++) {
-
         bracketsConfig.forEach(function (item, i, arr) {
-            if (str[iStr] === item[0]) {
-                // str[i] - открывающая
-
-// console.log( item );
-// console.log( 'str[i] = ' + str[i] );
-
-                stek.push(i); // кладем на стек
-            } else {
-                if (str[iStr] === item[1]) {
-                    // str[i] - закрывающая
-
-                    if (stek.length === 0) return false;
-
+            if (str[iStr] === item[0] && str[iStr] === item[1]) {
+                if (stek.length > 0) {
                     if (stek[stek.length - 1] === i) {
                         // правильный номер строки шаблона
 
                         stek.pop(); // снимаем со стека
+                    } else {
+                        stek.push(i); // кладем на стек
                     }
-                } else return false;
+                } else {
+                    stek.push(i); // кладем на стек
+                }
+            } else {
+                if (str[iStr] === item[0]) {
+                    // str[iStr] - открывающая
+
+                    stek.push(i); // кладем на стек
+                } else {
+                    if (str[iStr] === item[1]) {
+                        // str[iStr] - закрывающая
+
+                        if (stek.length === 0) return false;
+
+                        if (stek[stek.length - 1] === i) {
+                            // правильный номер строки шаблона
+
+                            stek.pop(); // снимаем со стека
+                        }
+                    } else return false;
+                }
             }
         });
-
-        /*
-      if( str[i] открывающая ) stek.push(номер строки шаблона);
-
-      if( str[i] закрывающая ){
-      if( stek.length === 0 )  return false;
-
-      if( stek[stek.length-1] === номер строки шаблона str[i] ){
-
-        stek.pop();
-      }else{
-        return false;
-*/
     }
 
     return stek.length === 0;
-
 };
-
-
